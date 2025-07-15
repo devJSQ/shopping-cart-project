03606ebbe5fdb8acea4507d72aba8ac0e8c7c504
JSQdev <148073798+devJSQ@users.noreply.github.com>
2025-07-15 04:34:04 +0300

    Update Node.java

--- src/Node.java
+++ src/Node.java
@@ -1,4 +1,22 @@
-public class Node {
-  
-  /* write your code here */
+
+class Node {
+    private Product product;
+    private Node next;
+
+    public Node(Product product) {
+        this.product = product;
+        this.next = null;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 }
